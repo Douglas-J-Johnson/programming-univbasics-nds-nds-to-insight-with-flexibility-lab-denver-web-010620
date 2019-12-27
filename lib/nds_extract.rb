e07ccc1a121b0d181413be783bb93dec203655f8
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-27 14:45:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -104,10 +104,13 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  pp source
-
+  director_data = {}
   director_name = ""
 
+  for i in 0...source.length
+    director_data = source[i]
+    director_name = director_data[:name]
+  end
 end
 
 # ----------------    End of Your Code Region --------------------
